0a4bc50152ee77471867f4c60c9d5cb43bddd6b9
zhaodacheng5 <zhaodacheng5@hikvision.com.cn>
2022-07-15 13:34:17 +0800

    feat: add button

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import TheWelcome from './components/TheWelcome.vue';
 </script>
 
 <template>
   <div id="app">
-    <header>
-      <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <SButton></SButton>
+    <!-- <header>
+      <img
+        alt="Vue logo"
+        class="logo"
+        src="./assets/logo.svg"
+        width="125"
+        height="125"
+      />
 
       <div class="wrapper">
         <HelloWorld msg="You did it!" />
@@ -15,7 +22,7 @@ import TheWelcome from './components/TheWelcome.vue'
 
     <main>
       <TheWelcome />
-    </main>
+    </main> -->
   </div>
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-import './assets/main.css'
+import './assets/main.css';
+import SButton from './ui/button/';
+
+Vue.use(SButton);
 
 new Vue({
-  render: (h) => h(App)
-}).$mount('#app')
+  render: h => h(App)
+}).$mount('#app');
